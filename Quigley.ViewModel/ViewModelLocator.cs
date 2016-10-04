908a106b67f6c909fd52a383cef3b6828f928511
MarcusQuigley <marcusquigley@gmail.com>
2016-10-04 16:56:16 -0400

    Inital checkin

---
+++ Quigley.ViewModel/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quigley.ViewModel
+{
+    public class ViewModelLocator
+    {
+        private static MainWindowViewModel mainWindowVM;
+
+        public static MainWindowViewModel MainWindowViewModelStatic
+        {
+            get
+            {
+                if (mainWindowVM == null)
+                {
+                    mainWindowVM = new MainWindowViewModel();
+                }
+                return mainWindowVM;
+            }
+        }
+    }
+}
